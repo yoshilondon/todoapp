318ada92f34c2878ed148b4ad1ffc9ec3e3b0c49
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-08-14 16:51:18 +0100

    userと記事の紐付けできた！

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,11 +8,11 @@ class BoardsController < ApplicationController
   end
   
   def new
-    @board = Board.new
+    @board = current_user.boards.build
   end
 
   def create
-    @board = Board.new(board_params)
+    @board = current_user.boards.build(board_params)
     if @board.save
       redirect_to board_path(@board)
     else
