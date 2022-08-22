482f1680491d755b7be5030068bfc6a928b07580
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-08-22 21:07:51 +0100

    annotateを導入

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Board < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
