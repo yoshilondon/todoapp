2d9045df4a1ce282575b152653708fd2616c5608
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-08-25 21:01:47 +0100

    validation length

--- app/models/board.rb
+++ app/models/board.rb
@@ -19,7 +19,10 @@
 #
 class Board < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 30 }
+
   validates :description, presence: true
+  validates :description, length: {minimum: 30, maximum: 140 }
 
   belongs_to :user
 
