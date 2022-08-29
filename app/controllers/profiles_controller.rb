71638d1494d143d0d7f578b9a92a665eca64c6c4
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-08-29 19:43:54 +0100

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,25 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'profile updated!'
+    else
+      flash.now[:error] = 'update failed...'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
 end
