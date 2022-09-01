6a7d77a6e5bb0f64fe097c149d0eb60ae3fc062c
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-09-01 22:55:13 +0100

    task create action!!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
     board = Board.find(params[:board_id])
     @task = board.tasks.build
   end
+
+  def create
+    board = Board.find(params[:board_id])
+    @task = board.tasks.build(task_params)
+    @task.user_id = current_user.id
+    if @task.save
+      redirect_to board_path(board), notice: 'add task'
+    else
+      flash.now[:error] = 'add task failed'
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -17,6 +17,8 @@
 #  index_tasks_on_user_id   (user_id)
 #
 class Task < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence: true
   validates :content, presence: true
   validates :content, uniqueness: true
